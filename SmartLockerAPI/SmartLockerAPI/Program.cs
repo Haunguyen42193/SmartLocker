1b2fd41d57498b8f09c697a21cce06f0cd25e544
Haunguyen42193 <haunguyen42193@gmail.com>
2023-09-15 16:16:46 +0700

    create web api

---
+++ SmartLockerAPI/SmartLockerAPI/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using SmartLocker.Data;
+using System.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<SmartLockerContext>(options =>
+    options.UseMySQL("Server=localhost;port=3307;Database=smart-locker;User Id=root;Password=0979620120@Hau;", mysqlOptions => mysqlOptions.EnableRetryOnFailure()));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
